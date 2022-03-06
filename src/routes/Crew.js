473f6ddc853b5262486ad604003621403f49d880
William L <a1d8cdb77@mailshld.com>
2022-03-06 19:48:25 +0000

    added crew page styles

--- src/routes/Crew.js
+++ src/routes/Crew.js
@@ -3,23 +3,32 @@ import Data from '../data.json';
 
 function Crew() {
   return (
-    <div>
-      <h3>
-        <span>02 </span>
-        MEET YOUR CREW
-      </h3>
-      <picture>
-        <source srcSet={Data.crew[0].images.webp} type="image/webp" />
-        <source srcSet={Data.crew[0].images.png} type="image/png" /> 
-        <img src={Data.crew[0].images.png} alt="crew"/>
-      </picture>
-      <nav>
-        <a href="/">1</a>
-      </nav>
-      <p>{Data.crew[0].role}</p>
-      <h4>{Data.crew[0].name}</h4>
-      <p>{Data.crew[0].bio}</p>
-    </div>
+    <>
+      <div className="crew-bg"></div>
+      <div className='grid-crew'>
+        <h3 className='grid-crew-header'>
+          <span>02 </span>
+          MEET YOUR CREW
+        </h3>
+        <picture className='grid-crew-picture'>
+          <source srcSet={Data.crew[0].images.webp} type="image/webp" />
+          <source srcSet={Data.crew[0].images.png} type="image/png" /> 
+          <img src={Data.crew[0].images.png} alt="crew"/>
+        </picture>
+        <nav className='grid-crew-nav'>
+          <button><span>{Data.crew[0].role}</span></button>
+          <button><span>{Data.crew[1].role}</span></button>
+          <button><span>{Data.crew[2].role}</span></button>
+          <button><span>{Data.crew[3].role}</span></button>
+        </nav>
+        <div className="grid-crew-text">
+          <p className='grid-crew-text-role'>{Data.crew[0].role}</p>
+          <h4 className='grid-crew-text-name'>{Data.crew[0].name}</h4>
+          <p className='grid-crew-text-bio'>{Data.crew[0].bio}</p>
+        </div>
+      </div>
+    </>
+
   )
 }
 
