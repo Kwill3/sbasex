996c73af3d7eb8ed6b189a7b1169cccb1a56f351
William L <a1d8cdb77@mailshld.com>
2022-03-10 22:37:39 +0000

    added style for destination page

--- src/routes/Destination.js
+++ src/routes/Destination.js
@@ -3,35 +3,58 @@ import Data from '../data.json';
 
 function Destination() {
 
-  return (
-    <div className='destination'>
-      <h3 className='destination-header'>01 PICK YOUR DESTINATION</h3>
-      <div className='destination-info'>
-        <picture className='destination-info-picture'>
-          <source srcSet={Data.destinations[0].images.webp} type="image/webp" />
-          <source srcSet={Data.destinations[0].images.png} type="image/png" /> 
-          <img src={Data.destinations[0].images.png} alt="Moon"/>
-        </picture>
-        <div className='destination-info-text'>
-          <nav className='destination-info-text-links'>
-            <a href="/">Moon</a>
-          </nav>
-          <h4 className='destination-info-text-title'>{Data.destinations[0].name}</h4>
-          <p className='destination-info-text-desc'>{Data.destinations[0].description}</p>
-          <div className='destination-info-text-data'>
-            <div className='destination-info-text-data-dist'>
-              <h5>AVG. DISTANCE</h5>
-              <p>{Data.destinations[0].distance}</p>
-            </div>
-            <div className='destination-info-text-data-time'>
-              <h5>EST. TRAVEL TIME</h5>
-              <p>{Data.destinations[0].travel}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+    <>
+    	<div className="dest-bg"></div>
+    	<div className='destination'>
+			<h3 className='destination-header'>
+				<span>01</span>
+				Pick your destination
+			</h3>
+			<div className='destination-info'>
+				<picture className='destination-info-picture'>
+					<source srcSet={Data.destinations[0].images.webp} type="image/webp" />
+					<source srcSet={Data.destinations[0].images.png} type="image/png" /> 
+					<img src={Data.destinations[0].images.png} alt="Moon"/>
+				</picture>
+				<div className='destination-info-text'>
+					<nav className='destination-info-text-links'>
+						<a href="/">Moon</a>
+						<a href="/">Mars</a>
+						<a href="/">Europa</a>
+						<a href="/">Titan</a>
+					</nav>
+					<h4 className='destination-info-text-title'>
+						{Data.destinations[0].name}
+					</h4>
+					<p className='destination-info-text-desc'>
+						{Data.destinations[0].description}
+					</p>
+					<hr />
+					<div className='destination-info-text-data'>
+						<div className='destination-info-text-data-dist'>
+							<h5>
+								AVG. DISTANCE
+							</h5>
+							<p>
+								{Data.destinations[0].distance}
+							</p>
+						</div>
+						<div className='destination-info-text-data-time'>
+							<h5>
+								EST. TRAVEL TIME
+							</h5>
+							<p>
+								{Data.destinations[0].travel}
+							</p>
+						</div>
+					</div>
+				</div>
+        	</div>
+      	</div>
+    </>
+
+  	)
 }
 
 export default Destination;
