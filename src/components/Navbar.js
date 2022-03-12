0d72d804ae55c58447de459f003b88d6f99bee2c
William L <a1d8cdb77@mailshld.com>
2022-03-12 09:34:52 +0000

    added hamburger function for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,26 +5,34 @@ function Navbar() {
     
     const activeNav = ({ isActive }) => isActive ? 'active' : 'inactive';
 
+    let handleClick = () => {
+        console.log('click');
+        document.getElementsByClassName("navbar-links")[0].classList.toggle('active');
+    }
+
     return (
         <div className='navbar'>
-            <div className="navbar-logo">
+            <div className="navbar-logo" onClick={handleClick}>
                 <img src="./assets/shared/logo.svg" alt="logo" />
             </div>
             <div className="navbar-links">
+                <div className="navbar-links-close" onClick={handleClick}>
+                    <img src="./assets/shared/icon-close.svg" alt="close" />
+                </div>
                 <NavLink className={activeNav} to="/">
-                    <span>00 </span>
+                    <span>00</span>
                     HOME
                 </NavLink>
                 <NavLink className={activeNav} to="/destination">
-                    <span>01 </span>
+                    <span>01</span>
                     DESTINATION
                 </NavLink>
                 <NavLink className={activeNav} to="/crew">
-                    <span>02 </span>
+                    <span>02</span>
                     CREW
                 </NavLink>
                 <NavLink className={activeNav} to="/technology">
-                    <span>03 </span>
+                    <span>03</span>
                     TECHNOLOGY
                 </NavLink>
             </div>
